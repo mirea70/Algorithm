2d574a810743ca705a8f453bf0037df183b07eb8
mirea70 <deae670@daum.net>
2026-01-04 14:48:10 +0900

    binary_search: LeetCode Top 150 Q35_SearchInsertPosition Solve.

---
+++ src/leetcode/top150/binarysearch/Q35_SearchInsertPosition.java
@@ -0,0 +1,23 @@
+package leetcode.top150.binarysearch;
+
+public class Q35_SearchInsertPosition {
+    public int searchInsert(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length-1;
+
+        while(start <= end) {
+            int mid = (start + end) / 2;
+            if(nums[mid] == target) {
+                return mid;
+            }
+            else if(target > nums[mid]) {
+                start = mid + 1;
+            }
+            else {
+                end = mid-1;
+            }
+        }
+
+        return start;
+    }
+}
