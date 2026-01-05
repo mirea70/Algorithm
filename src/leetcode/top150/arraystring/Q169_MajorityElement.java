d0e0ca5eed153c4d7298d155589794ccc63cc6c9
mirea70 <deae670@daum.net>
2026-01-05 15:47:12 +0900

    arraystring: leetcode Q169_MajorityElement Solve.

---
+++ src/leetcode/top150/arraystring/Q169_MajorityElement.java
@@ -0,0 +1,21 @@
+package leetcode.top150.arraystring;
+
+public class Q169_MajorityElement {
+    public int majorityElement(int[] nums) {
+        int candidate = 0, count = 0;
+
+        for(int num : nums) {
+            if(count == 0) {
+                candidate = num;
+                count = 1;
+            }
+            else if(num == candidate) {
+                count++;
+            }
+            else {
+                count--;
+            }
+        }
+        return candidate;
+    }
+}
