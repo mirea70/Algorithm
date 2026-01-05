fc1841df062c6be17b9517f093687acb92477c2f
mirea70 <deae670@daum.net>
2026-01-05 21:57:47 +0900

    arraystring: leetcode Q28_FindTheIndexOfTheFirstOccurrenceInAString Solve.

---
+++ src/leetcode/top150/arraystring/Q28_FindTheIndexOfTheFirstOccurrenceInAString.java
@@ -0,0 +1,7 @@
+package leetcode.top150.arraystring;
+
+public class Q28_FindTheIndexOfTheFirstOccurrenceInAString {
+    public int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+}
